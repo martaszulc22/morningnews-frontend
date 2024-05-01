837f3f0cb6777c70d5b344c93dcc1e639a4e4ff9
Marta Szulc <marta.szulc2202@gmail.com>
2024-05-01 18:48:24 +0200

    first commit

---
+++ reducers/hiddenArticles.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    value: [],
+};
+
+export const hiddenArticlesSlice = createSlice({
+    name: 'hiddenArticles',
+    initialState,
+    reducers: {
+        hideArticle: (state, action) => {
+            console.log('Add hidden article', action.payload)
+            state.value.push(action.payload);
+        },
+        unhideArticles: (state) => {
+            state.value = [];
+        }
+    },
+});
+
+export const { hideArticle, unhideArticles } = hiddenArticlesSlice.actions;
+export default hiddenArticlesSlice.reducer;
